9cb0adec7bf6f68492d7a73d4c5af0c8bf6a509a
0e1arquitetos <141720410+0e1arquitetos@users.noreply.github.com>
2024-06-07 12:04:40 -0300

    3d

---
+++ data.js
@@ -0,0 +1,330 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-estar---a",
+      "name": "ESTAR - A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.036249146002960586,
+        "pitch": 0.014796026612856394,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35380296110949416,
+          "pitch": 0.3752922322425487,
+          "rotation": 0,
+          "target": "2-cozinha---a"
+        },
+        {
+          "yaw": -0.28047794077361843,
+          "pitch": 1.3238350810400377,
+          "rotation": 0,
+          "target": "1-estar---b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-estar---b",
+      "name": "ESTAR - B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3590145273082186,
+          "pitch": 0.3761976445855719,
+          "rotation": 0,
+          "target": "3-cozinha---b"
+        },
+        {
+          "yaw": 0.2912544380987203,
+          "pitch": 0.384071124647015,
+          "rotation": 0,
+          "target": "4-corredor"
+        },
+        {
+          "yaw": -0.15984953260125678,
+          "pitch": 1.3271817153534116,
+          "rotation": 0,
+          "target": "0-estar---a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cozinha---a",
+      "name": "COZINHA - A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.0009920865227837083,
+        "pitch": 0.05867221185804539,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.988227893443648,
+          "pitch": 0.4745577617068122,
+          "rotation": 0,
+          "target": "0-estar---a"
+        },
+        {
+          "yaw": -2.292455359223011,
+          "pitch": 1.5223218850366553,
+          "rotation": 0,
+          "target": "3-cozinha---b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cozinha---b",
+      "name": "COZINHA - B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.0013425609630726143,
+        "pitch": 0.05548509979821148,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0027425559272922,
+          "pitch": 0.48910688632738264,
+          "rotation": 0,
+          "target": "1-estar---b"
+        },
+        {
+          "yaw": 2.88453533255492,
+          "pitch": 1.4967864941027305,
+          "rotation": 0,
+          "target": "2-cozinha---a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-corredor",
+      "name": "CORREDOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0.01208320468352575,
+        "pitch": -0.001849503326610602,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3393780141714444,
+          "pitch": 0.5042236825361393,
+          "rotation": 0,
+          "target": "6-quarto-02"
+        },
+        {
+          "yaw": 3.061503029745648,
+          "pitch": 0.20624830779603798,
+          "rotation": 0,
+          "target": "0-estar---a"
+        },
+        {
+          "yaw": -3.1120674403581567,
+          "pitch": 0.202759253423487,
+          "rotation": 0,
+          "target": "1-estar---b"
+        },
+        {
+          "yaw": -2.7618193251084406,
+          "pitch": 0.5325990885472365,
+          "rotation": 0,
+          "target": "5-quarto-01"
+        },
+        {
+          "yaw": 0.020958037642005323,
+          "pitch": 0.41608584172720064,
+          "rotation": 0,
+          "target": "7-suite"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-quarto-01",
+      "name": "QUARTO 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-quarto-02",
+      "name": "QUARTO 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5704481078938937,
+          "pitch": 0.7393633923526828,
+          "rotation": 0,
+          "target": "4-corredor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-suite",
+      "name": "SUITE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -3.107949932788589,
+        "pitch": -0.026517554189275927,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.505527502228004,
+          "pitch": 0.18798674071804022,
+          "rotation": 0,
+          "target": "4-corredor"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "MPK - EP V02",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
